5be83268185b8db163aa388e42a6d28774d90573
SABoev20 <SABoev20@codingburgas.bg>
2024-08-08 18:11:27 +0300

    Create projects for auth and resource server

---
+++ server/resource/src/test/java/project/aurora/resource/ResourceApplicationTests.java
@@ -0,0 +1,13 @@
+package project.aurora.resource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
